47254d8cbf4bb80c4430c3c3a27d659a4a706253
jibraan98 <jibraan2010@hotmail.com>
2024-03-30 11:16:48 +0400

    second commit

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { OrganizationList } from '@clerk/nextjs'
+import { OrganizationList } from "@clerk/nextjs";
 
 export default function CreateOrganizationPage() {
-    return (
-        <OrganizationList 
-            hidePersonal
-            afterSelectOrganizationUrl={'/organization/:id'}
-            afterCreateOrganizationUrl={'/organization/:id'}
-        />
-    )
-}
+  return (
+    <OrganizationList
+      hidePersonal
+      afterSelectOrganizationUrl="/organization/:id"
+      afterCreateOrganizationUrl="/organization/:id"
+    />
+  );
+};
---
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+export type Organization = {
+    id: string
+    slug: string
+    imageUrl: string
+    name: string
+}
+
+interface NavItemProps {
+    isExpanded: boolean;
+    isActive: boolean;
+    organization: Organization;
+    onExpand: (id: string) => void;
+}
+
+export const NavItem = ({isExpanded, isActive, organization, onExpand}: NavItemProps) => {
+    return (
+        <div>
+            Navitem
+        </div>
+    )
+}
